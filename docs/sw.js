104df50d78fbed11089acf6925d2ff8d612af0c0
Daniel Schep <dschep@gmail.com>
2018-04-10 20:21:52 -0400

    public-url for parcel

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
 parcelRequire=function(e,r,n){var t="function"==typeof parcelRequire&&parcelRequire,i="function"==typeof require&&require;function u(n,o){if(!r[n]){if(!e[n]){var f="function"==typeof parcelRequire&&parcelRequire;if(!o&&f)return f(n,!0);if(t)return t(n,!0);if(i&&"string"==typeof n)return i(n);var c=new Error("Cannot find module '"+n+"'");throw c.code="MODULE_NOT_FOUND",c}a.resolve=function(r){return e[n][1][r]||r};var l=r[n]=new u.Module(n);e[n][0].call(l.exports,a,l,l.exports)}return r[n].exports;function a(e){return u(a.resolve(e))}}u.isParcelRequire=!0,u.Module=function(e){this.id=e,this.bundle=u,this.exports={}},u.modules=e,u.cache=r,u.parent=t;for(var o=0;o<n.length;o++)u(n[o]);return u}({14:[function(require,module,exports) {
 self.addEventListener("push",function(t){var i=t.data?t.data.json():{};t.waitUntil(self.registration.showNotification(i.title,{body:i.message,icon:"ntfy.png"}))});
 },{}]},{},[14])
-//# sourceMappingURL=/sw.map
+//# sourceMappingURL=sw.map
