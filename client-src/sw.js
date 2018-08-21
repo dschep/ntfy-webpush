ec4bf9a146a0bb0020ead36b864f83f5d631fb42
Evaluating <f1510809@mvrht.com>
2018-08-21 15:49:03 -0400

    fix icon link in sw.js for parcel

--- client-src/sw.js
+++ client-src/sw.js
@@ -1,9 +1,11 @@
+import icon from './ntfy.png';
+
 self.addEventListener('push', function(event) {
   var payload = event.data ? event.data.json() : {};
   event.waitUntil(
     self.registration.showNotification(payload.title, {
       body: payload.message,
-      icon: 'ntfy.png',
+      icon,
     })
   );
 });
